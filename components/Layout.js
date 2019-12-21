f5d6f5eb58616da85c4a271559a9039011d5f125
Best <irb.ossystem@gmail.com>
2019-12-22 00:06:02 +0200

    PWA images setu[

--- components/Layout.js
+++ components/Layout.js
@@ -37,6 +37,14 @@ class Layout extends Component {
         <Head>
           <title>Trumptr</title>
           <link href="https://fonts.googleapis.com/css?family=Karla&display=swap" rel="stylesheet" />
+
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="theme-color" content="#007bff" />
+          <link rel="apple-touch-icon" href="/static/icon-512x512.png" />
+          <meta name="apple-mobile-web-app-title" content="Trumptr" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
         </Head>
         <Navbar user={user} logout={this.logout} />
         <AppProvider value={{
